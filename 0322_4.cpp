21641f09509faa2621cdc7ea9c18fbb711423f49
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-22 15:24:18 +0900

    행렬의 곱 step3 (완성)

---
+++ 0322_4.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	int A[2][2]={
+	{3,4},
+	{5,6}
+	};
+	int B[2][2]={
+	{1,5},
+	{3,2}
+	};
+	//z값을 어디까지 돌릴지 구하기 위한 계산 
+	int colA=sizeof(A[0])/sizeof(int);
+	
+	int AB[2][2]={0};
+	//행렬의 곱 구하기 + 출력 
+	printf("AB = \n"); 
+	for (int i=0;i<2;i++){
+		for (int j=0;j<2;j++){
+			for (int z=0;z<colA;z++){
+				AB[i][j] += A[i][z]*B[z][j];
+			}
+			printf("%d ",AB[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
